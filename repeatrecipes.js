41190e501c9b961ddbc6eb32911d52e900394606
jasarzen <108990065+jasarzen@users.noreply.github.com>
2022-11-21 12:31:47 -0500

    Adding VSC

---
+++ repeatrecipes.js
@@ -0,0 +1,99 @@
+function show(){
+    alert("Sign-Up for Emails");
+}
+
+////// UPPER LEFT
+function changeUpperLeftPicture(){
+   document.getElementById("upperLeft").style.backgroundImage = "url('applepie.webp')";
+}
+
+function backUpperLeftPicture(){
+   document.getElementById("upperLeft").style.backgroundImage = "url('apple.jpg')";
+}
+
+function clickUpperLeftPicture(){
+     document.getElementById("upperLeft").style.backgroundImage = "url('apple.html')";
+  }
+
+////// UPPER MIDDLE
+
+function changeUpperMiddlePicture(){
+    document.getElementById("upperMiddle").style.backgroundImage = "url('blueberrypie.jpg')";
+}
+
+function backUpperMiddlePicture(){
+    document.getElementById("upperMiddle").style.backgroundImage = "url('blueberries.jpg')";
+}
+
+////// UPPER Right
+function changeUpperRightPicture(){
+    document.getElementById("upperRight").style.backgroundImage = "url('cherrypie.jpg')";
+}
+
+function backUpperRightPicture(){
+    document.getElementById("upperRight").style.backgroundImage = "url('cherries.jpg')";   
+}
+
+////// MIDDLE LEFT
+
+function changeMiddleLeftPicture(){
+    document.getElementById("middleLeft").style.backgroundImage = "url('peachpie.jpg')";
+}
+
+function backMiddleLeftPicture(){
+    document.getElementById("middleLeft").style.backgroundImage = "url('peaches.jpg')";   
+}
+
+//////// MIDDLE MIDDLE
+
+function changeMiddleMiddlePicture(){
+    document.getElementById("middleMiddle").style.backgroundImage = "url('keylimepie.avif')";
+}
+
+function backMiddleMiddlePicture(){
+    document.getElementById("middleMiddle").style.backgroundImage = "url('keylimes.jpg')";   
+}
+
+//////// MIDDLE RIGHT
+
+function changeMiddleRightPicture(){
+    document.getElementById("middleRight").style.backgroundImage = "url('pumkinpie.jpg')";
+}
+
+function backMiddleRightPicture(){
+    document.getElementById("middleRight").style.backgroundImage = "url('pumpkin.png')";   
+}
+
+
+////// BOTTOM LEFT
+
+function changeBottomLeftPicture(){
+    document.getElementById("bottomLeft").style.backgroundImage = "url('pecanpie.avif')";
+}
+
+function backBottomLeftPicture(){
+    document.getElementById("bottomLeft").style.backgroundImage = "url('pecans.jpg')";   
+}
+
+//////// BOTTOM MIDDLE
+
+function changeBottomMiddlePicture(){
+    document.getElementById("bottomMiddle").style.backgroundImage = "url('coconutpie.jpg')";
+}
+
+function backBottomMiddlePicture(){
+    document.getElementById("bottomMiddle").style.backgroundImage = "url('coconuts.jpg')";   
+}
+
+//////// BOTTOM RIGHT
+
+function changeBottomRightPicture(){
+    document.getElementById("bottomRight").style.backgroundImage = "url('strawberrypie.webp')";
+}
+
+function backBottomRightPicture(){
+    document.getElementById("bottomRight").style.backgroundImage = "url('strawberries.jpg')";   
+}
+
+
+
